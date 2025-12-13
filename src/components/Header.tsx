6538f4be1c1da7cafc23867d62f84be88a56c92a
Brylle Justin Heraldo <bryllejheraldo@gmail.com>
2025-12-13 16:46:41 +0800

    border for the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,22 +3,22 @@ import profileDP from "../assets/profileDP.jpg";
 
 const Header: React.FC = () => {
   return (
-  <header className="text-black py-5 header-border">
-    <div className="container">
-      <div className="d-flex flex-column flex-lg-row align-items-center justify-content-center text-center gap-4">
-        
-        <div>
-          <h1 className="display-4 fw-bold">Brylle Justin Heraldo</h1>
-          <p className="lead">
-            Full Stack Developer | Game Developer | 3D Artist
-          </p>
+    <header className="text-black py-5">
+      <div className="container">
+        <div className="col-lg-10 mx-auto header-border">
+          <div className="d-flex flex-column flex-lg-row align-items-center justify-content-center text-center gap-4">
+            <div>
+              <h1 className="display-4 fw-bold">Brylle Justin Heraldo</h1>
+              <p className="lead">
+                Full Stack Developer | Game Developer | 3D Artist
+              </p>
+            </div>
+
+            <img src={profileDP} alt="Profile" className="small-profile" />
+          </div>
         </div>
-
-        <img src={profileDP} alt="Profile" className="small-profile" />
       </div>
-    </div>
-  </header>
-
+    </header>
   );
 };
 
