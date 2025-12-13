c07847c070f6f820d01b08881f34016112e0286b
Brylle Justin Heraldo <bryllejheraldo@gmail.com>
2025-12-13 22:22:41 +0800

    about me fix

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,42 +7,50 @@ const About: React.FC = () => {
         <div className="row">
           <div className="col-lg-8 mx-auto text-center">
             <h2 className="mb-4">About Me</h2>
-            <p className="lead">
-              I'm a passionate developer with experience in creating web
-              applications. I love turning ideas into reality through code and
-              design.
-            </p>
-            <p>
-              With a background in computer science and a keen interest in
-              emerging technologies, I strive to build efficient, user-friendly
-              solutions that make a difference.
-            </p>
+            <div className="card mb-4">
+              <div className="card-body">
+                <p className="lead">
+                  I'm a passionate developer with experience in creating web
+                  applications. I love turning ideas into reality through code
+                  and design.
+                </p>
+                <p>
+                  With a background in computer science and a keen interest in
+                  emerging technologies, I strive to build efficient,
+                  user-friendly solutions that make a difference.
+                </p>
+              </div>
+            </div>
 
             {/* Tech Stacks Section */}
             <div className="mt-5">
               <h3 className="mb-4">Technical Skills</h3>
-              <div className="row">
-                <div className="col-md-6">
-                  <h5 className="text-primary">Frontend</h5>
-                  <ul className="list-unstyled">
-                    <li>• React.js / Next.js</li>
-                    <li>• TypeScript / JavaScript (ES6+)</li>
-                    <li>• HTML5 / CSS3 / Sass</li>
-                    <li>• Bootstrap / Tailwind CSS</li>
-                    <li>• Redux / Context API</li>
-                    <li>• Vue.js</li>
-                  </ul>
-                </div>
-                <div className="col-md-6">
-                  <h5 className="text-primary">Backend</h5>
-                  <ul className="list-unstyled">
-                    <li>• Node.js / Express.js</li>
-                    <li>• Python / Django / Flask</li>
-                    <li>• Java / Spring Boot</li>
-                    <li>• MongoDB / PostgreSQL</li>
-                    <li>• RESTful APIs / GraphQL</li>
-                    <li>• Docker / Kubernetes</li>
-                  </ul>
+              <div className="card">
+                <div className="card-body">
+                  <div className="row">
+                    <div className="col-md-6">
+                      <h5 className="text-primary">Frontend</h5>
+                      <ul className="list-unstyled">
+                        <li>• React.js / Next.js</li>
+                        <li>• TypeScript / JavaScript (ES6+)</li>
+                        <li>• HTML5 / CSS3 / Sass</li>
+                        <li>• Bootstrap / Tailwind CSS</li>
+                        <li>• Redux / Context API</li>
+                        <li>• Vue.js</li>
+                      </ul>
+                    </div>
+                    <div className="col-md-6">
+                      <h5 className="text-primary">Backend</h5>
+                      <ul className="list-unstyled">
+                        <li>• Node.js / Express.js</li>
+                        <li>• Python / Django / Flask</li>
+                        <li>• Java / Spring Boot</li>
+                        <li>• MongoDB / PostgreSQL</li>
+                        <li>• RESTful APIs / GraphQL</li>
+                        <li>• Docker / Kubernetes</li>
+                      </ul>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
